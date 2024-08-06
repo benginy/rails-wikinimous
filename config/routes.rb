3ccde4f32df51aeedcdad84b5dc540773d4aa08f
Bengin Yilmaz <141424348+benginy@users.noreply.github.com>
2024-08-06 16:37:41 +0200

    W6D2 Exercice 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get "/articles", to: "articles#index"
+  # get "/articles/new", to: "articles#new", as: :new_article
+  # post "/articles", to: "articles#create"
+  # get "/articles/:id", to: "articles#show", as: :article
+  # get "/articles/:id/edit", to: "articles#edit", as: :edit_article
+  # patch "/articles/:id", to: "articles#update"
+  # delete "/articles/:id", to: "articles#destroy"
+  resources :articles
+  # resources :articles, only: [:show]
+  # resources :articles, except: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
